1c7d1f6e0e2bf430b2bb94160ab2c9f138c03409
Abhishekh3007 <116192759+Abhishekh3007@users.noreply.github.com>
2024-02-15 10:32:50 +0000

    new file:   01_basics/test1.js 	new file:   01_basics/variables.js

---
+++ 01_basics/variables.js
@@ -0,0 +1,11 @@
+const acountId=145236;
+let acountEmail="abhi10@gmail.com";
+var acountPassword="123456";//do not use var because of block scope
+acountCity="banglore";
+
+//acountId=12121;not  allowed 
+acountEmail="abc@gmail.com"
+console.log(acountEmail);
+let acountName;
+
+console.table([acountId,acountEmail,acountPassword,acountCity,acountName]);
