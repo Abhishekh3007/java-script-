49bf3c7fba20f95ce3f286ab7aff1757d3f03bbb
Abhishekh3007 <116192759+Abhishekh3007@users.noreply.github.com>
2024-02-17 07:25:35 +0000

    new file:   01_basics/dataTypes.js

---
+++ 01_basics/dataTypes.js
@@ -0,0 +1,30 @@
+"use strict"; // All the js codes are in newer version
+
+//alert(3 + 3); //error because we are using node and syntax is not this
+
+console.log(3 + 3); //redablity of code is most important as is this line is telling that it is adding and displaying
+
+console.log("abhishek");
+console.log("abhi"); //this is wrong practise of writing the code as its not readable it will run but prefer not writing the code like this
+
+//premitive 
+let name="abhishek";//as its bounded into double qoutes it would be iedentified as string
+console.log(typeof "name");//typeof is used to check the datatype 
+let age=22;//now its an number or int data type
+let isLoggedIn=false;//boolean value either true or false
+
+//number => 2 power 53 is the space adress alocated to the number or intger data type
+//bigint => mostly used in big sites or huge number type ex facebook stock market
+//string=""
+//null=> its is an stand alone value most important 
+//undefined => its an  variable which not defined just initialized and not asigned anything
+let state;//undefined output
+console.log(state);
+let  temprature=null; //null output
+console.log(temprature);
+
+//symbol=> to identified unique as in react you build multiple componets you can identify uniqely
+
+//object 
+console.log(typeof null);//output is object null is an standalone value 
+console.log(typeof undefined);//output is undefined its an type
