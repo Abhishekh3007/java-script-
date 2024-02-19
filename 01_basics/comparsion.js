2ac4b9b95ffce8ba5423c193cd723c18282b5fc9
Abhishekh3007 <116192759+Abhishekh3007@users.noreply.github.com>
2024-02-19 07:31:50 +0000

    new file:   01_basics/comparsion.js 	new file:   01_basics/datatypes_summary.js
    commit;

---
+++ 01_basics/comparsion.js
@@ -0,0 +1,23 @@
+//comparsion and equallity of operators
+console.log("2" > 1);//true because string is convertedto number and then compared to the next value
+// console.log(2 > 1);
+// console.log(2 >= 1);
+// console.log(2 < 1);
+// console.log(2 == 1);
+// console.log(2 != 1);
+
+
+// console.log("2" > 1);
+// console.log("02" > 1);
+
+console.log(null > 0);
+console.log(null == 0);
+console.log(null >= 0);
+
+console.log(undefined == 0);
+console.log(undefined > 0);
+console.log(undefined < 0);
+
+// === 
+
+console.log("2" === 2);
---
+++ 01_basics/datatypes_summary.js
@@ -0,0 +1,5 @@
+//primitive data types
+// 7 types of primitve data types => String boolena Null undefined number  symbol
+
+//refrences type or (non premitive data types)
+//Arrays objects functions
