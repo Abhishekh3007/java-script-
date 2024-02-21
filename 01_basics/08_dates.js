116737459e9ef7f7f84f1f4731be7f8ff91bd6ef
Abhishekh3007 <116192759+Abhishekh3007@users.noreply.github.com>
2024-02-21 11:31:34 +0000

    new file:   01_basics/08_dates.js 	modified:   01_basics/08_dates.js commit

---
+++ 01_basics/08_dates.js
@@ -0,0 +1,12 @@
+let myDate=new Date();
+console.log(myDate.toString());
+console.log(myDate.toLocaleString());
+
+
+let myDate1=new Date("2023-02-01")
+console.log(myDate1);
+console.log(myDate1.getTime());
+
+let myTimeStamp=Date.now();
+console.log(myTimeStamp);
+console.log(Math.floor(Date.now()/1000))
